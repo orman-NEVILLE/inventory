4360386a0b42baf766875b46c30ed2692d35bc4d
ORMAN <21mk386@esisalama.org>
2024-05-31 11:19:17 +0200

    Dans le corps de la classe, déclarez une fonction abstraite qui renvoie ItemDao afin que la base de données puisse identifier le DAO.

--- app/src/main/java/com/example/inventory/data/InventoryDatabase.kt
+++ app/src/main/java/com/example/inventory/data/InventoryDatabase.kt
@@ -4,4 +4,6 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 
 @Database(entities = [Item::class], version = 1, exportSchema = false)
-abstract class InventoryDatabase : RoomDatabase() {}
+abstract class InventoryDatabase : RoomDatabase() {
+    abstract fun itemDao(): ItemDao
+}
