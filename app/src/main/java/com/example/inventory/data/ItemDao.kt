a54cdf51865ac32e93357b6652858f8e01a09627
ORMAN <21mk386@esisalama.org>
2024-05-31 10:37:48 +0200

    @Delete

--- app/src/main/java/com/example/inventory/data/ItemDao.kt
+++ app/src/main/java/com/example/inventory/data/ItemDao.kt
@@ -1,11 +1,18 @@
 package com.example.inventory.data
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Update
 
 @Dao
 interface ItemDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(item: Item)
+    @Update
+    suspend fun update(item: Item)
+    @Delete
+    suspend fun delete(item: Item)
+
 }
